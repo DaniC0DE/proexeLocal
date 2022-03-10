e514478591ddb1c7deff27cb40e0c5d3077df37b
Daniel Alvarado <danieltrm1810@gmail.com>
2022-03-10 18:42:49 -0300

    Se crea redux.js con actions, reducer y action creators, manejo de accion LOAD, creando getUsers para traer la data del endpoint y su posterior uso mediante el action creator loadingUsers

---
+++ src/redux.js
@@ -0,0 +1,51 @@
+const initialState = {
+  users: [],
+};
+
+// Actions
+const LOAD = "LOAD";
+const ADD = "ADD";
+const EDIT = "EDIT";
+const DELETE = "DELETE";
+
+// Reducer
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    // do reducer stuff
+    case LOAD:
+      return { ...state, users: action.users };
+    default:
+      return state;
+  }
+}
+
+// Action Creators
+export function loadingUsers(users) {
+  return { type: LOAD, users };
+}
+
+export function addUser(userToAdd) {
+  return { type: ADD, userToAdd };
+}
+
+export function editUser(userToEdit) {
+  return { type: EDIT, userToEdit };
+}
+
+export function deleteUser(userToDelete) {
+  return { type: DELETE, userToDelete };
+}
+
+// side effects, only as applicable
+// e.g. thunks, epics, etc
+export function getUsers() {
+  return (dispatch) => {
+    fetch(
+      "https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => dispatch(loadingUsers(users)));
+  };
+}
